b93a05b55ce1cb703b9699be8d7ec188124105a0
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-16 16:42:05 -0400

    added shader compilation functionality

--- Include/TKVulkan.h
+++ Include/TKVulkan.h
@@ -2,11 +2,15 @@
 #define TKVUL_MAIN_H
 
 #include <stdint.h>
+#define __need_size_t
+#include <stddef.h>
 
 bool tkvul_connect(const char *name, uint32_t version);
 
 bool tkvul_connectSurface(void **data);
 
 bool tkvul_initialize(uint32_t framebufferWidth, uint32_t framebufferHeight);
+
+bool tkvul_compileShaders(const char **names, size_t count);
 
 #endif // TKVUL_MAIN_H
---
+++ Source/Shaders.c
@@ -0,0 +1,113 @@
+#include <TKAssets.h>
+#include <TKVulkan.h>
+#include <glslang/Include/glslang_c_interface.h>
+#include <glslang/Public/resource_limits_c.h>
+#include <stdio.h>
+#include <string.h>
+
+// TODO: Separate this into shared library or something so we can annihalate
+// TODO: the GLSLANG dep once finished.
+
+bool tkvul_compileShaders(const char **names, size_t count)
+{
+    glslang_initialize_process();
+
+    for (size_t i = 0; i < count; i++)
+    {
+        char *currentName = strdup(names[i]);
+
+        const char *const filename = strtok(currentName, ".");
+        if (filename == nullptr)
+        {
+            printf("File '%s' has no extension and therefore no stage can be "
+                   "assumed.\n",
+                   currentName);
+            free(currentName);
+            return false;
+        }
+        char *extension = strtok(nullptr, ".");
+
+        glslang_input_t input = {
+            .language = GLSLANG_SOURCE_GLSL,
+            .stage = GLSLANG_STAGE_VERTEX,
+            .client = GLSLANG_CLIENT_VULKAN,
+            .client_version = GLSLANG_TARGET_VULKAN_1_4,
+            .target_language = GLSLANG_TARGET_SPV,
+            .target_language_version = GLSLANG_TARGET_SPV_1_6,
+            .code = nullptr,
+            .default_version = 460,
+            .default_profile = GLSLANG_CORE_PROFILE,
+            .force_default_version_and_profile = false,
+            .forward_compatible = false,
+            .messages = GLSLANG_MSG_DEFAULT_BIT,
+            .resource = glslang_default_resource(),
+        };
+
+        if (strcmp(extension, "frag") == 0)
+            input.stage = GLSLANG_STAGE_FRAGMENT;
+
+        tkast_file_t file = {0};
+        file.name = filename;
+        if (input.stage == GLSLANG_STAGE_VERTEX) file.type = TKAST_VERTEX_FILE;
+        if (input.stage == GLSLANG_STAGE_FRAGMENT)
+            file.type = TKAST_FRAGMENT_FILE;
+        tkast_loadFile(&file);
+        input.code = (const char *)file.content;
+
+        glslang_shader_t *shader = glslang_shader_create(&input);
+        if (!glslang_shader_preprocess(shader, &input))
+        {
+            puts("Failed to preprocess shader.");
+            free(file.content);
+            free(currentName);
+            return false;
+        }
+        if (!glslang_shader_parse(shader, &input))
+        {
+            puts("Failed to parse shader.");
+            free(file.content);
+            free(currentName);
+            return false;
+        }
+
+        glslang_program_t *program = glslang_program_create();
+        glslang_program_add_shader(program, shader);
+        if (!glslang_program_link(program, GLSLANG_MSG_SPV_RULES_BIT |
+                                               GLSLANG_MSG_VULKAN_RULES_BIT))
+        {
+            puts("Failed to link shader.");
+            free(file.content);
+            free(currentName);
+            return false;
+        }
+        glslang_program_SPIRV_generate(program, input.stage);
+        if (glslang_program_SPIRV_get_messages(program))
+        {
+            puts("Failed to get SPIRV messages for shader.");
+            free(file.content);
+            free(currentName);
+            return false;
+        }
+
+        char outputPath[tkast_maxPathLength];
+        sprintf(outputPath, "%s-%s", filename,
+                (input.stage == GLSLANG_STAGE_FRAGMENT ? "frag" : "vert"));
+
+        tkast_file_t outputFile = {
+            .name = outputPath,
+            .type = TKAST_SPIRV_FILE,
+            .size =
+                glslang_program_SPIRV_get_size(program) * sizeof(unsigned int),
+        };
+
+        tkast_writeFile(
+            &outputFile,
+            (const uint8_t *)glslang_program_SPIRV_get_ptr(program));
+
+        free(file.content);
+        free(currentName);
+    }
+
+    glslang_finalize_process();
+    return true;
+}
