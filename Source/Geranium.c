b77986c36b2b6f1d5ddb34d37d08e999335a5e2f
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-26 15:16:26 -0400

    renamed file structure to geranium

--- Source/Geranium.c
+++ Source/Geranium.c
@@ -1,4 +1,4 @@
-#include <WLVulkan.h>
+#include <Geranium.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- Source/Shaders.c
+++ Source/Shaders.c
@@ -1,6 +1,6 @@
 #define AGERATUM_IMPLEMENTATION
 #include <Ageratum.h>
-#include <WLVulkan.h>
+#include <Geranium.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
